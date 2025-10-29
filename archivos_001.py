25437f93967e284d108d1149e05b44119703b2b2
Mercado Mario Leonardo <mercaco21@gmail.com>
2025-10-29 19:12:12 -0300

    Create archivos_001.py

---
+++ archivos_001.py
@@ -0,0 +1,9 @@
+# Escritura de archivo de texto
+
+archivo = open ('./datos/archivo'.txt,'w' )
+archivo.write ('linea1\n')
+archivo.write ('linea2\n')
+archivo.write ('linea2\n')
+
+archivo.close()
+
